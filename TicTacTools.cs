84847e6895633660ebf1c4ac47d02a3789af2aba
whitekaden75 <130481320+whitekaden75@users.noreply.github.com>
2025-01-27 09:50:23 -0700

    Update TicTacTools.cs

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -16,7 +16,7 @@ namespace Mission4
             }
         }
         // method to find a winner if there is one.
-        public bool ChooseWinner(string[,] board)
+        public bool ChooseWinner(string[,] board, string player1, string player2)
         {
             
             //checking rows for winner
@@ -24,29 +24,56 @@ namespace Mission4
             {
                 if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != null)
                 {
-                    Console.WriteLine(board[i, 0] + "is the winner");
-                    return true; 
+                    if (board[i,0] == "X")
+                    {
+                        Console.WriteLine(player1 + "is the winner");
+                    }
+                    else
+                    {
+                        Console.WriteLine(player2 + "is the winner");
+                    } 
                 }
             }
             //checking columns for winner
             for (int i = 0; i < 3; i++)
             {
-                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != null)
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
                 {
-                    Console.WriteLine(board[0, 1] + "is the winner");
+                    if (board[0, i] == "X")
+                    {
+                        Console.WriteLine(player1 + "is the winner");
+                    }
+                    else
+                    {
+                        Console.WriteLine(player2 + "is the winner");
+                    }
                     return true; 
                 }
             }
             // check first diaganal for winner
-            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != null)
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
             {
-                Console.WriteLine(board[0, 0] + "is the winner");
+                if (board[0, 0] == "X")
+                {
+                    Console.WriteLine(player1 + "is the winner");
+                }
+                else
+                {
+                    Console.WriteLine(player2 + "is the winner");
+                }
                 return true; 
             }
             // check second diaganal for winner
             if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != null)
             {
-                Console.WriteLine(board[0, 2] + "is the winner");
+                if (board[0,2] == "X")
+                {
+                    Console.WriteLine(player1 + "is the winner");
+                }
+                else
+                {
+                    Console.WriteLine(player2 + "is the winner");
+                }
                 return true; 
             }
             return false;
