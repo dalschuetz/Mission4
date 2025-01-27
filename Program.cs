8c51824042925845bdda6e5a9f0120b9a3b05f35
dalschuetz <131074445+dalschuetz@users.noreply.github.com>
2025-01-27 08:38:20 -0700

    Part 1 Program

--- Program.cs
+++ Program.cs
@@ -2,4 +2,37 @@
 
 TicTacTools ttt = new TicTacTools();
 
+/*Welcome User to the Game
+ * Create board array
+ * Ask player for choice and update array
+ * Print board
+ * Call choose winner method and notify when a win has occured
+ * */
 
+string[,] Board = new string[3,3];
+Board[0, 0] = "A1";
+Board[0, 1] = "A2";
+Board[0, 2] = "A3";
+Board[1, 0] = "B1";
+Board[1, 1] = "B2";
+Board[1, 2] = "B3";
+Board[2, 0] = "C1";
+Board[2, 1] = "C2";
+Board[2, 2] = "C3";
+bool gameOver = false;
+
+//Print initial board
+
+do
+{ 
+    //Ask player 1 for choice and update
+
+    //print new board
+
+    //Ask player 2 for choice and update
+
+    //print new board
+
+    //Check if there's a winner
+
+} while (!gameOver);
--- TicTacTools.cs
+++ TicTacTools.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ Receive board array
+Prints Board based on information passed
+Receive Board array input and returns if there's a winner
+ */
+
 namespace Mission4
 {
     internal class TicTacTools
     {
+        public void RecieveBoard()
+        {
+
+        }
+
+        public void PrintBoard()
+        {
+
+        }
+
+        public void ChooseWinner()
+        {
+
+        }
     }
 }
